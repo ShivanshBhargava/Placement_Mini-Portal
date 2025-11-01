fbdc0998171436439d1bf5d22cab7ee0a68752db
ShivanshBhargava <shivanshbhargavaofficial@gmail.com>
2025-11-01 20:52:03 +0530

    added animations and improved lander

--- frontend/placement_mini_portal/src/components/Header/Header.jsx
+++ frontend/placement_mini_portal/src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import './Header.css';
 import Navigation from "./Navigation";
 import AuthButtons from "./AuthButtons";
 import logo from '../../assets/Logo.svg';
+import { motion } from "framer-motion"
 
 
 function Header() {
@@ -11,7 +12,21 @@ function Header() {
 
   return (
         <div className="Header">
-            <div style={{width:"17vw", display:"flex"}}><img src={logo} alt="My logo" className="Logo"/></div>
+            
+            <div style={{width:"14vw", display:"flex", border:"solid black "}}>
+            <motion.img src={logo}
+            whileHover={{
+              scale: 1.1,
+              opacity: 0.6,
+              transition: { duration: 0.1, ease: "easeOut" },
+            }}
+            alt="My Logo"
+            className="Logo"
+            transition={{
+              duration: 0.2,
+              ease: "easeInOut", 
+            }}/>
+            </div>
             <Navigation className="Nav"/>
             <AuthButtons className="Butt"/>
         </div>
--- frontend/placement_mini_portal/src/components/Landing_page/Hero.jsx
+++ frontend/placement_mini_portal/src/components/Landing_page/Hero.jsx
@@ -2,15 +2,63 @@
 import React from "react";
 import './Hero.css';
 import Header from "../Header/Header";
+import Laptop from '../../assets/Laptop.svg'
+import logo from '../../assets/Logo.svg'
+import { motion } from "framer-motion"
+
 
 
 function Hero() {
-  const name = "World";
-
+  const ball = {
+    width: 100,
+    height: 100,
+    backgroundColor: "#5686F5",
+    borderRadius: "50%",
+}
   return (
-    <div className="blackbg">
-        <Header/>
-        <h1 className="question">Ready to level up your career?</h1>
+    <div>
+      <div className="blackbg">
+          <Header/>
+          <motion.img
+            src={logo}
+
+            style={{
+              width: "50vw",
+              height: "auto",
+              margin: "auto",
+              zIndex: 1,
+            }}
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{
+              opacity: [0, 1, 1, 0], // fade in → stay → fade out
+              scale: [0.8, 1, 1, 0.8], // zoom in → stay → zoom out
+            }}
+            transition={{
+              duration: 2, // total time (in seconds)
+              times: [0, 0.3, 0.7, 1], // timing of keyframes
+              ease: "easeInOut",
+            }}
+          />
+          <motion.img
+          src={Laptop}
+          alt="Laptop"
+          className="Laptop"
+          style={{
+            marginTop:"10vh",
+            zIndex: 2,
+            position: "absolute",
+          }}
+          initial={{ opacity: 0, y: 50, scale: 0.9 }}
+          animate={{ opacity: 1, y: 0, scale: 1 }}
+          transition={{
+            delay:1.7,
+            duration: 1.7,
+            ease: "easeOut",
+            type: "spring",
+            bounce: 0.3,
+          }}
+        />
+      </div>
     </div>
   );
 }
